1c07cf06ff6769a77bb2365018c31d4c06c8b695
Diplav Kushwaha <diplavkushwaha@gmail.com>
2025-07-04 14:53:14 +0530

    Update D2_ArrayCheckSameNumber.java

--- D2_ArrayCheckSameNumber.java
+++ D2_ArrayCheckSameNumber.java
@@ -22,10 +22,33 @@ class CheckSameNumber{
         }
     }
 }
+class CheckLastOccurrence{
+    Scanner sc=new Scanner(System.in);
+    public void CheckOccurrence(int[] arr){
+        System.out.print("\nEnter number for check :");
+        int n=sc.nextInt();
+        int count = 0;
+        boolean check=false;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==n){
+                count=i+1;
+                check=true;
+            }
+        }
+        if(check){
+            System.out.println("Number "+n+" present at "+count+"th position");
+        }
+        else{
+            System.out.println("Number "+n+" is not present in array");
+        }
+    }
+}
 public class D2_ArrayCheckSameNumber {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         CheckSameNumber checkSameNumber=new CheckSameNumber();
+        CheckLastOccurrence checkLastOccurrence=new CheckLastOccurrence();
+
 
         System.out.print("Enter size of array :");
         int n=sc.nextInt();
@@ -35,5 +58,6 @@ public class D2_ArrayCheckSameNumber {
             arr[i]=sc.nextInt();
         }
         checkSameNumber.CountOfSameNumber(arr);
+        checkLastOccurrence.CheckOccurrence(arr);
     }
 }
