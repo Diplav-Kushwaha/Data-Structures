25e7e0283f7116da740185c822d7b2e641e3d13d
Diplav Kushwaha <diplavkushwaha@gmail.com>
2025-07-08 10:44:18 +0530

    Update D3_ArrayProblemSolving.java

--- D3_ArrayProblemSolving.java
+++ D3_ArrayProblemSolving.java
@@ -2,7 +2,10 @@ package DataStructures;
 
 //1. Target Sum problem: Sum of Pairs, Sum of Triplets
 //2. Array Manipulation : Find unique numbers in array
-//3. Second Max Number: Find Second maximum number in array
+//3. Second Maximum Number: 
+//4. Reverse of Array:
+//5. Rotation of Array:
+
 import java.util.Scanner;
 class TargetSumProblem{
     Scanner sc=new Scanner(System.in);
@@ -73,12 +76,54 @@ class SecondMaximumNumber{
         System.out.println(secMaxNumber);
     }
 }
+class ReverseAnArray{
+    public void SwapArray(int[] arr, int i, int j){
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public void ReverseArray(int[] arr){
+        System.out.println("Reversing an array========================");
+        int i=0, j=arr.length-1;
+        while(i<j){
+            SwapArray(arr, i, j);
+            i++;
+            j--;
+        }
+        System.out.print(">>>Reverse Array :");
+        for(int k=0; k<arr.length; k++){
+            System.out.print(arr[k]+" ");
+        }
+    }
+}
+class RotateAnArray{
+    public void Rotation(int[] arr){
+        System.out.println("Rotation of an array========================");
+        Scanner sc=new Scanner(System.in);
+        System.out.print(">>>Enter no. of rotation :");
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++){
+            int temp=arr[0];
+            for(int j=0; j<arr.length-1; j++){
+                arr[j]=arr[j+1];
+            }
+            arr[arr.length-1]=temp;
+        }
+        System.out.print(">>>Rotated Array :");
+        for(int k=0; k<arr.length; k++){
+            System.out.print(arr[k]+" ");
+        }
+
+    }
+}
 public class D3_ArrayProblemSolving {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         TargetSumProblem targetSumProblem=new TargetSumProblem();
         ArrayManipulation arrayManipulation=new ArrayManipulation();
         SecondMaximumNumber secondMaximumNumber=new SecondMaximumNumber();
+        ReverseAnArray reverseAnArray=new ReverseAnArray();
+        RotateAnArray rotateAnArray=new RotateAnArray();
 
         System.out.print(">>>Enter size of array :");
         int n=sc.nextInt();
@@ -90,5 +135,7 @@ public class D3_ArrayProblemSolving {
         targetSumProblem.TargetSum(arr);
         arrayManipulation.Manipulation(arr);
         secondMaximumNumber.SecondMaximum(arr);
+        reverseAnArray.ReverseArray(arr);
+        rotateAnArray.Rotation(arr);
     }
 }
